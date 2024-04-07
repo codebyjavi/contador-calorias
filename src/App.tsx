2394cfaa634ff6fb67992ca22f0cbfbdd4cba2f0
javierspr <learn.javier@gmail.com>
2024-04-07 18:08:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+      <header className="py-8 bg-green-600">
+        <div className="max-w-5xl mx-auto flex justify-between items-center">
+          <h1 className="text-white uppercase font-bold text-2xl">Contador de calorías</h1>
+          <button className="px-6 py-3 bg-black text-white uppercase font-bold rounded-md">Reinciar app</button>
+        </div>
+      </header>
+
+      <section className="bg-green-400">
+        <div className="max-w-5xl mx-auto py-20">
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
