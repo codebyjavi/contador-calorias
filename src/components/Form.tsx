22e915d167bee8e87261dd7844f2fc3188b65281
javierspr <learn.javier@gmail.com>
2024-04-08 14:22:32 -0300

    add form state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 export default function Form() {
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
   return (
     <form
         className="space-y-5 bg-white shadow p-10 rounded-md"
@@ -8,23 +15,29 @@ export default function Form() {
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categoría:</label>
             <select 
-             className="border border-slate-300 p-2 rounded-md w-full bg-white"
-             id="category"
+                className="border border-slate-300 p-2 rounded-md w-full bg-white"
+                id="category"
+                value={activity.category}
             >
                 {categories.map(category => (
-                    <option key={category.id}>
+                    <option 
+                        key={category.id}
+                        value={category.id}
+                    >
                         {category.name}
                     </option>
                 ))}
             </select>
         </div>
+
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <label htmlFor="name" className="font-bold">Actividad:</label>
             <input 
+                id="name"
                 type="text"
-                id="activity"
                 className="border border-slate-300 p-2 rounded-md"
                 placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Ensalada, etc..."
+                value={activity.name}
             />
         </div>
 
@@ -35,6 +48,7 @@ export default function Form() {
                 id="calories"
                 className="border border-slate-300 p-2 rounded-md"
                 placeholder="Ej. 300 o 500"
+                value={activity.calories}
             />
         </div>
 
