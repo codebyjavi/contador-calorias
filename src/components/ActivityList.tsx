fa5e68c558f3ff18c8a12f53a28ea0d0cc8f2e05
javierspr <learn.javier@gmail.com>
2024-05-15 23:41:18 -0300

    update categoryName

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,10 +1,18 @@
 import { Activity } from "../types"
+import { categories } from "../data/categories"
+import { useMemo } from "react"
 
 type ActivityListProps = {
   activities: Activity[]
 }
 
 export default function ActivityList({ activities } : ActivityListProps) {
+  
+  const categoryName = useMemo(() => 
+    (category: Activity['category']) =>
+      categories.map( cat => cat.id === category ? cat.name : '')
+    , [activities])
+  
   return (
     <>
       <h2 className='text-4xl font-bold text-slate-600 text-center mt-10'>Comida y Actividades</h2>
@@ -12,7 +20,7 @@ export default function ActivityList({ activities } : ActivityListProps) {
         <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
           <div className="space-y-2 relative">
            <p>
-             {activity.category}
+             {categoryName(+activity.category)}
            </p>
            <p className="text-2xl font-bold pt-5">
             {activity.name}
